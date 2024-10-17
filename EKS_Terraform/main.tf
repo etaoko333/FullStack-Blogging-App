c7f545f08bad959946d4cb53603416a2c1d59a92
etaoko333 <131201117+etaoko333@users.noreply.github.com>
2024-10-17 07:39:16 +0100

    Update main.tf

--- EKS_Terraform/main.tf
+++ EKS_Terraform/main.tf
@@ -2,60 +2,55 @@ provider "aws" {
   region = "us-east-2"
 }
 
-# VPC
-resource "aws_vpc" "vpc" {
+resource "aws_vpc" "devopsola_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "web-app"
+    Name = "devopsola-vpc"
   }
 }
 
-# Subnet
-resource "aws_subnet" "public_subnet_az1" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.0.0/24"
-  availability_zone       = "us-east-2a"
+resource "aws_subnet" "devopsola_subnet" {
+  count = 2
+  vpc_id                  = aws_vpc.devopsola_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.devopsola_vpc.cidr_block, 8, count.index)
+  availability_zone       = element(["us-east-2a", "us-east-2b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "public subnet Az1"
+    Name = "devopsola-subnet-${count.index}"
   }
 }
 
-# Internet Gateway
-resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id = aws_vpc.vpc.id
+resource "aws_internet_gateway" "devopsola_igw" {
+  vpc_id = aws_vpc.devopsola_vpc.id
 
   tags = {
-    Name = "web-IG"
+    Name = "devopsola-igw"
   }
 }
 
-# Route Table
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.vpc.id
+resource "aws_route_table" "devopsola_route_table" {
+  vpc_id = aws_vpc.devopsola_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internet_gateway.id
+    gateway_id = aws_internet_gateway.devopsola_igw.id
   }
 
   tags = {
-    Name = "public route table"
+    Name = "devopsola-route-table"
   }
 }
 
-# Route Table Association
-resource "aws_route_table_association" "public_subnet_az1_route_table_association" {
-  subnet_id      = aws_subnet.public_subnet_az1.id
-  route_table_id = aws_route_table.public_route_table.id
+resource "aws_route_table_association" "a" {
+  count          = 2
+  subnet_id      = aws_subnet.devopsola_subnet[count.index].id
+  route_table_id = aws_route_table.devopsola_route_table.id
 }
 
-# Security Group for EKS Cluster
-resource "aws_security_group" "web_cluster_sg" {
-  name   = "dev-sg"
-  vpc_id = aws_vpc.vpc.id
+resource "aws_security_group" "devopsola_cluster_sg" {
+  vpc_id = aws_vpc.devopsola_vpc.id
 
   egress {
     from_port   = 0
@@ -65,13 +60,12 @@ resource "aws_security_group" "web_cluster_sg" {
   }
 
   tags = {
-    Name = "web-cluster-sg"
+    Name = "devopssola-cluster-sg"
   }
 }
 
-# Security Group for EKS Nodes
-resource "aws_security_group" "web_node_sg" {
-  vpc_id = aws_vpc.vpc.id
+resource "aws_security_group" "devopsola_node_sg" {
+  vpc_id = aws_vpc.devopsola_vpc.id
 
   ingress {
     from_port   = 0
@@ -88,26 +82,25 @@ resource "aws_security_group" "web_node_sg" {
   }
 
   tags = {
-    Name = "web-node-sg"
+    Name = "devopsola-node-sg"
   }
 }
 
-# EKS Cluster
 resource "aws_eks_cluster" "devopsola" {
   name     = "devopsola-cluster"
   role_arn = aws_iam_role.devopsola_cluster_role.arn
 
   vpc_config {
-    subnet_ids         = [aws_subnet.public_subnet_az1.id]
-    security_group_ids = [aws_security_group.web_cluster_sg.id]
+    subnet_ids         = aws_subnet.devopsola_subnet[*].id
+    security_group_ids = [aws_security_group.devopsola_cluster_sg.id]
   }
 }
 
-# EKS Node Group
 resource "aws_eks_node_group" "devopsola" {
+  cluster_name    = aws_eks_cluster.devopsola.name
   node_group_name = "devopsola-node-group"
   node_role_arn   = aws_iam_role.devopsola_node_group_role.arn
-  subnet_ids      = [aws_subnet.public_subnet_az1.id]
+  subnet_ids      = aws_subnet.devopsola_subnet[*].id
 
   scaling_config {
     desired_size = 3
@@ -118,13 +111,12 @@ resource "aws_eks_node_group" "devopsola" {
   instance_types = ["t2.large"]
 
   remote_access {
-    ec2_ssh_key = "project2"  # Provide your SSH key name here
-    source_security_group_ids = [aws_security_group.web_node_sg.id]
+    ec2_ssh_key = var.ssh_key_name
+    source_security_group_ids = [aws_security_group.devopsola_node_sg.id]
   }
 }
 
-# IAM Role for EKS Cluster
-resource "aws_iam_role" "devopsola_cluster_role" {
+resource "aws_iam_role" "devopssola_cluster_role" {
   name = "devopsola-cluster-role"
 
   assume_role_policy = <<EOF
@@ -143,13 +135,11 @@ resource "aws_iam_role" "devopsola_cluster_role" {
 EOF
 }
 
-# Attach Policy to Cluster Role
 resource "aws_iam_role_policy_attachment" "devopsola_cluster_role_policy" {
   role       = aws_iam_role.devopsola_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-# IAM Role for EKS Node Group
 resource "aws_iam_role" "devopsola_node_group_role" {
   name = "devopsola-node-group-role"
 
@@ -169,7 +159,6 @@ resource "aws_iam_role" "devopsola_node_group_role" {
 EOF
 }
 
-# Attach Policies to Node Group Role
 resource "aws_iam_role_policy_attachment" "devopsola_node_group_role_policy" {
   role       = aws_iam_role.devopsola_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
